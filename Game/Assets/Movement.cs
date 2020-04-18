3939f8ffa985b475f32a1702782a373b0b3cf574
urwinater <58256147+urwinater@users.noreply.github.com>
2020-04-18 09:17:54 +0100

    sandbox scene

---
+++ Game/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 movement;
+    public Rigidbody2D player;
+    public float MovementSpeed = 5f;
+    void Update()
+    {
+        movement.x =  Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        
+    }
+    void FixedUpdate()
+    {
+        player.MovePosition(player.position + movement * MovementSpeed * Time.fixedDeltaTime);
+    }
+}
